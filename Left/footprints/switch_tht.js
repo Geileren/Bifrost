f9da9a16b9af9e3d4bf4e63e9288600acc21f934
Geileren <jakob.geil@gmail.com>
2026-03-28 15:19:36 +0100

    finish left?

--- Left/footprints/switch_tht.js
+++ Left/footprints/switch_tht.js
@@ -8,8 +8,8 @@ module.exports = {
     (module switch_tht (layer F.Cu) (tedit 0)
       ${p.at}
       (fp_text reference "${p.ref}" (at 0 0) (layer F.SilkS) hide (effects (font (size 1 1) (thickness 0.15))))
-      (pad 1 thru_hole circle (at -2.5 0) (size 2.5 2.5) (drill 1.2) (layers *.Cu *.Mask) ${p.from.str})
-      (pad 2 thru_hole circle (at 2.5 0) (size 2.5 2.5) (drill 1.2) (layers *.Cu *.Mask) ${p.to.str})
+      (pad 1 thru_hole circle (at -1.27 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.from.str})
+      (pad 2 thru_hole circle (at 1.27 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.to.str})
     )
   `
 }
